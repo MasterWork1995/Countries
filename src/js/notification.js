e8ba94874eef82887602e4af51ab7d1de5611ad3
Yurii Yurents <yura19950117@gmail.com>
2021-06-28 15:35:08 +0300

    fix notification

--- src/js/notification.js
+++ src/js/notification.js
@@ -1,11 +1,18 @@
-import { alert, defaultModules } from '../../node_modules/@pnotify/core/dist/PNotify.js';
+import { error, defaultModules } from '../../node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '../../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import "@pnotify/confirm/dist/PNotifyConfirm.css";
+
 
 defaultModules.set(PNotifyMobile, {});
 
+
+
 function showMessage() {
-  alert({
-    text: "Too many matches founds. Please enter a more specific query!"
+  error({
+    text: 'Too many matches found. Please enter a more specific query!',
 });
 };
 
